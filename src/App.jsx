6adc6a2f9d0a2600cf341d1cac0a34622cf55bf0
bubucuo <276185876@qq.com>
2025-08-26 23:05:30 -0700

    save todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { createStore } from "solid-js/store";
+import styles from "./App.module.css";
+
+const getId = () => {
+  return Math.random().toString(36).substr(2, 9);
+};
 
 function App() {
+  const [store, setStore] = createStore([
+    { key: getId(), name: "name0" },
+    { key: getId(), name: "name1" },
+  ]);
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <button
+        style={{
+          cursor: "pointer",
+        }}
+        onClick={() =>
+          setStore([...store, { key: getId(), name: "name" + store.length }])
+        }
+      >
+        add
+      </button>
+      <ul class={styles.container}>
+        {store.map((item, index) => (
+          <li key={item.key}>
+            <input
+              type="text"
+              value={item.name}
+              onInput={(e) => setStore(index, "name", e.target.value)}
+            />
+            <button
+              onClick={() => setStore(store.filter((_, i) => i !== index))}
+            >
+              delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { createStore } from "solid-js/store";
+
+// Initialize store
+ const [store, setStore] = createStore([
+    {key: '0',name:'name0'},
+    {key: '1',name:'name1'}
+]);
