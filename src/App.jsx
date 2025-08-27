130f177c077c158fa5d4d76cb4a1fb5ae04eb640
bubucuo <276185876@qq.com>
2025-08-26 23:34:55 -0700

    add table todolist

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,21 @@ const getId = () => {
   return Math.random().toString(36).substr(2, 9);
 };
 
+const createItems = (row, column) => {
+  return {
+    key: getId(),
+    items: Array.from({ length: column }, (_, i) => ({
+      key: getId(),
+      text: "item" + row + "-" + i,
+    })),
+  };
+};
+
 function App() {
   const [store, setStore] = createStore([
-    { key: getId(), name: "name0" },
-    { key: getId(), name: "name1" },
+    createItems(0, 2),
+    createItems(1, 1),
+    createItems(2, 3),
   ]);
   return (
     <div class={styles.App}>
@@ -16,28 +27,64 @@ function App() {
         style={{
           cursor: "pointer",
         }}
-        onClick={() =>
-          setStore([...store, { key: getId(), name: "name" + store.length }])
-        }
+        onClick={() => setStore([...store, { key: getId(), items: [] }])}
       >
         add
       </button>
-      <ul class={styles.container}>
-        {store.map((item, index) => (
-          <li key={item.key}>
-            <input
-              type="text"
-              value={item.name}
-              onInput={(e) => setStore(index, "name", e.target.value)}
-            />
-            <button
-              onClick={() => setStore(store.filter((_, i) => i !== index))}
-            >
-              delete
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div class={styles.container}>
+        {store.map((item, rowIndex) => {
+          return (
+            <ul class={styles.row} key={item.key}>
+              <li>
+                {rowIndex}
+                <button
+                  onClick={() =>
+                    setStore(rowIndex, "items", [
+                      ...item.items,
+                      {
+                        key: getId(),
+                        text: "item" + rowIndex + "-" + item.items.length,
+                      },
+                    ])
+                  }
+                >
+                  add column
+                </button>
+              </li>
+              {item.items.map((subItem) => {
+                return (
+                  <li key={subItem.key}>
+                    <input
+                      type="text"
+                      value={subItem.text}
+                      onInput={(e) =>
+                        setStore(
+                          rowIndex,
+                          "items",
+                          subItem.key,
+                          "text",
+                          e.target.value
+                        )
+                      }
+                    />
+                    <button
+                      onClick={() =>
+                        setStore(
+                          rowIndex,
+                          "items",
+                          item.items.filter((i) => i.key !== subItem.key)
+                        )
+                      }
+                    >
+                      delete item
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
 }
